a2904e8a3d27c8f3506cd2bf4e21403e17cf4f32
Daniel Ravndal <danielr@coretrek.no>
2024-06-18 15:31:22 +0200

    hallelujah

--- init.lua
+++ init.lua
@@ -1,10 +1,5 @@
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-
--- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
@@ -18,6 +13,3 @@ require("lazy-bootstrap")
 
 -- [[ Configure and install plugins ]]
 require("lazy-plugins")
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
