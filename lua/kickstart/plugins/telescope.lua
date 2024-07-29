770a97c4fc0c09512e2e0646dda4317022ec683f
danielr <danielr@coretrek.no>
2024-07-29 06:42:10 +0200

    nuts

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -28,6 +28,8 @@ return {
 						path_display = {
 							"filename_first",
 						},
+						prompt_prefix = ' 󰍉 ',
+						selection_caret = '  ',
 					},
 				},
 				extensions = {
@@ -78,10 +80,81 @@ return {
 				})
 			end, { desc = "[S]earch [/] in Open Files" })
 
+
 			-- Shortcut for searching your Neovim configuration files
 			vim.keymap.set("n", "<leader>sn", function()
 				builtin.find_files({ cwd = vim.fn.stdpath("config") })
 			end, { desc = "[S]earch [N]eovim files" })
+
+			local actions = require('telescope.actions')
+			local action_state = require('telescope.actions.state')
+			local pickers = require('telescope.pickers')
+			local finders = require('telescope.finders')
+			local conf = require('telescope.config').values
+			local builtin = require('telescope.builtin')
+			local themes = require('telescope.themes')
+
+			local function open_find_files(prompt_bufnr)
+				local selection = action_state.get_selected_entry()
+				actions.close(prompt_bufnr)
+				if selection then
+					builtin.find_files({
+						prompt_title = "Files in " .. selection.display,
+						cwd = selection.value,
+					})
+				end
+			end
+
+			local function directory_picker()
+				local root_dir = "/home/danielr/Work/Projects"
+
+				local results = {}
+				local job_output = vim.fn.systemlist('find "' .. root_dir .. '" -maxdepth 1 -type d')
+				for _, path in ipairs(job_output) do
+					if path ~= root_dir then
+						local dir_name = vim.fn.fnamemodify(path, ":t")
+						table.insert(results, {dir_name, path})
+					end
+				end
+
+				local custom_theme = themes.get_dropdown({
+					winblend = 10,
+					width = 0.5,
+					previewer = false,
+					prompt_title = '  Select Project ',
+					border = true,
+					borderchars = {
+						prompt = {"─", "│", " ", "│", "╭", "╮", "│", "│"},
+						results = {"─", "│", "─", "│", "├", "┤", "╯", "╰"},
+						preview = {"─", "│", "─", "│", "╭", "╮", "╯", "╰"},
+					},
+					prompt_prefix = '  ',
+					selection_caret = '  ',
+				})
+
+				pickers.new(custom_theme, {
+					prompt_title = "Select Directory",
+					finder = finders.new_table {
+						results = results,
+						entry_maker = function(entry)
+							return {
+								value = entry[2],  -- full path
+								display = entry[1],  -- directory name
+								ordinal = entry[1],  -- for sorting
+							}
+						end,
+					},
+					sorter = conf.generic_sorter(opts),
+					attach_mappings = function(prompt_bufnr, map)
+						map('i', '<CR>', open_find_files)
+						map('n', '<CR>', open_find_files)
+						return true
+					end,
+				}):find()
+			end
+
+			vim.api.nvim_create_user_command('PickDirectory', directory_picker, {})
+			vim.keymap.set("n", "<leader>sp", ":PickDirectory<CR>" , { desc = "[S]earch [P]rojects" })
 		end,
 	},
 }
