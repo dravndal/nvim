0b82b3a1c9a07f970f13c8badabac2d2b87b489a
dravndal <danielcravndal@gmail.com>
2025-05-08 23:13:05 +0200

    fix(mini): remove unused mini plugin

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,46 +1,45 @@
 return {
-	{
-		"echasnovski/mini.diff",
-		version = false,
-		opts = {},
-	},
-	{
-		"echasnovski/mini.snippets",
-		version = false,
-		config = function()
-			local gen_loader = require("mini.snippets").gen_loader
-			require("mini.snippets").setup({
-				snippets = {
-					gen_loader.from_file("~/.config/nvim/snippets/global.json"),
-					gen_loader.from_lang(),
-				},
-			})
-		end,
-	},
-	{
-		"echasnovski/mini.statusline",
-		version = false,
-		opts = {},
-	},
-	{
-		"echasnovski/mini.comment",
-		version = false,
-		opts = {},
-	},
-	{
-		"echasnovski/mini.pairs",
-		version = false,
-		opts = {},
-	},
-	{
-		"echasnovski/mini.surround",
-		version = false,
-		opts = {},
-	},
-	{
-		"echasnovski/mini.ai",
-		version = false,
-		opts = {},
-	},
+  {
+    "echasnovski/mini.diff",
+    version = false,
+    opts = {},
+  },
+  {
+    "echasnovski/mini.snippets",
+    version = false,
+    config = function()
+      local gen_loader = require("mini.snippets").gen_loader
+      require("mini.snippets").setup({
+        snippets = {
+          gen_loader.from_file("~/.config/nvim/snippets/global.json"),
+          gen_loader.from_lang(),
+        },
+      })
+    end,
+  },
+  {
+    "echasnovski/mini.statusline",
+    version = false,
+    opts = {},
+  },
+  {
+    "echasnovski/mini.comment",
+    version = false,
+    opts = {},
+  },
+  {
+    "echasnovski/mini.pairs",
+    version = false,
+    opts = {},
+  },
+  {
+    "echasnovski/mini.surround",
+    version = false,
+    opts = {},
+  },
+  {
+    "echasnovski/mini.ai",
+    version = false,
+    opts = {},
+  },
 }
--- vim: ts=2 sts=2 sw=2 et
